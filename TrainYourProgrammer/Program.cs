1e2239247cd78eec74fd1c3bd2267a41d79a08cd
Kay Jänke <kjaehnke@netik.de>
2017-10-11 13:40:15 +0200

    Initial Commit

---
+++ TrainYourProgrammer/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TrainYourProgrammer
+{
+    using System;
+
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.Write(Math.PI);
+            Console.Read();
+        }
+    }
+}
