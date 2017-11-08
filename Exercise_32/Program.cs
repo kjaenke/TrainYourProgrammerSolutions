29fb246d8322141182afc00966c4eca13878deab
Kay Jänke <kjaehnke@netik.de>
2017-11-08 12:36:44 +0100

    Exercise _32 u _34

---
+++ Exercise_32/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Exercise_32
+{
+    using System;
+
+    internal static class Program
+    {
+        private static void Main()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("1. Zahl : ");
+                var first = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+                Console.Write("Operator: ");
+                var Operator = Console.ReadLine();
+                Console.Write("2. Zahl : ");
+                var second = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+                switch (Operator)
+                {
+                    case "+" :
+                        Console.WriteLine($"Ergebnis: {first + second}");
+                        break;
+                    case "-" :
+                        Console.WriteLine($"Ergebnis: {first - second}");
+                        break;
+                    case "/" :
+                        Console.WriteLine($"Ergebnis: {first / second}");
+                        break;
+                    case "*" :
+                        Console.WriteLine($"Ergebnis: {first * second}");
+                        break;
+                }
+                Console.WriteLine("Drücken Sie eine beliebige Taste...");
+                Console.Read();
+            }
+        }
+    }
+}
